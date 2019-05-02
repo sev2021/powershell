000f69468d45c6de456da9d2e5c8e417c188921a
code <33802433+codeacade@users.noreply.github.com>
2019-05-02 01:41:15 +0100

    <h2> changed into <h3>

--- configure-nginx2.sh
+++ configure-nginx2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Set the home page.
-echo "<html><body><h2>Welcome to Azzure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index2.html
+echo "<html><body><h3>Welcome to Azzure! My name is $(hostname).</h3></body></html>" | sudo tee -a /var/www/html/index2.html
